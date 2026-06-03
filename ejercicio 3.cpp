8d40dbca1fbf60c3eadf985c3cd510a34de26a7d
bastardomontanobarbaradelvalle-max <bastardomontanobarbaradelvalle@gmail.com>
2026-06-03 15:55:22 -0400

    Add files via upload

---
+++ ejercicio 3.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int x= 0;
+int y= 0;	
+	
+	
+cout<< "introduce las coordenadas X y Y";
+cin>> x, y;
+
+if(x == 0 && y == 0){
+cout<< "cuadrante cero\n";
+}
+else if( x == 0 && y == 0){
+cout<<"cuadrante 3\n";
+}
+else if(x == 0 && y == 0){
+	cout<<"cuadrante 1\n";
+}
+else if( x == 0 && y == 0){
+	
+	cout<<"cuadrante 2\n";
+	
+}
+
+else{
+
+cout<<"otro cuadrante\n";
+}
+ return 0;
+
+}
